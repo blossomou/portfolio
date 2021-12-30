8200bfac9722a05fa2ec656eaa8c77feef19d48a
QingOu <qingqingou@gmail.com>
2021-12-30 16:23:35 -0500

    next js fundamentals

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,9 @@
-const index = () => {
+import { GetStaticPropsContext } from 'next';
+
+import { services } from '../data';
+
+const index = ({ services }) => {
+  console.log('CLIENT', services);
   return (
     <div>
       <h1>bye bye world</h1>
@@ -7,3 +12,29 @@ const index = () => {
 };
 
 export default index;
+
+// export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+//   //calculation
+//   const res = await fetch('http://localhost:3000/api/services');
+//   const data = await res.json();
+
+//   console.log('SERVER', services);
+//   return {
+//     props: {
+//       services: data.services,
+//     },
+//   };
+// };
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
+  //calculation
+  const res = await fetch('http://localhost:3000/api/services');
+  const data = await res.json();
+
+  console.log('SERVER', services);
+  return {
+    props: {
+      services: data.services,
+    },
+  };
+};
