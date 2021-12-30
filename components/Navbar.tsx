c9153d56512b13061ff7517a2e0baafa754fd6c9
QingOu <qingqingou@gmail.com>
2021-12-30 12:04:52 -0500

    Add Navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,51 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { FunctionComponent, useEffect, useState } from 'react';
+
+interface NavProps {
+  activeItem: string;
+  setActiveItem: Function;
+  name: string;
+  route: string;
+}
+
+const NavItem: FunctionComponent<NavProps> = ({ activeItem, setActiveItem, route, name }) => {
+  return activeItem !== name ? (
+    <Link href={route}>
+      <a>
+        <span
+          className="hover:text-custom_green"
+          onClick={() => {
+            setActiveItem(name);
+          }}
+        >
+          {name}
+        </span>
+      </a>
+    </Link>
+  ) : null;
+};
+
+const Navbar = () => {
+  const [activeItem, setActiveItem] = useState<string>('');
+
+  const { pathname } = useRouter();
+  useEffect(() => {
+    if (pathname === '/') setActiveItem('About');
+    if (pathname === '/projects') setActiveItem('Projects');
+    if (pathname === '/resume') setActiveItem('Resume');
+  }, []);
+
+  return (
+    <div className="flex justify-between px-5 py-3 my-3">
+      <span className="text-xl font-bold border-b-4 text-custom_green border-custom_green md:text-2xl">{activeItem} </span>
+      <div className="flex space-x-5 text-lg">
+        <NavItem activeItem={activeItem} setActiveItem={setActiveItem} name="About" route="/" />
+        <NavItem activeItem={activeItem} setActiveItem={setActiveItem} name="Projects" route="/projects" />
+        <NavItem activeItem={activeItem} setActiveItem={setActiveItem} name="Resume" route="/resume" />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
